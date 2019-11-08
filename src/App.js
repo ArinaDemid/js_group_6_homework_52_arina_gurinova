d4cd2ebfa7e36da00d6b2e7df6f2bfcbb4e3a513
Arina Gurinova <arina.gurinova@gmail.com>
2019-11-09 05:55:15 +0600

    Создала метод generateNumbers

--- src/App.js
+++ src/App.js
@@ -13,6 +13,20 @@ class App extends Component {
       {number: showNumber[4]}
     ]
   };
+
+  generateNumbers = () => {
+    let numbers = [];
+    while (numbers.length < 5) {
+      let item = Math.floor(Math.random() * 32 + 5);
+      if (numbers.indexOf(item) === -1) {
+        numbers.push(item);
+      };
+    }
+    let result = numbers.sort(function(a, b) {
+      return a - b;
+    });
+    return result;
+  }
   
   render() {
     return (
