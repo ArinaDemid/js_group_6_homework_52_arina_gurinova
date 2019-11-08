d6ebc422b9048eb6d9d07a668e7fb17a17d9c0f1
Arina Gurinova <arina.gurinova@gmail.com>
2019-11-09 05:44:08 +0600

    Добавила компонент

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import Circle from './components/circle';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    numbers: [
+      {number: 1},
+      {number: 2},
+      {number: 3},
+      {number: 4},
+      {number: 5}
+    ]
+  };
+  
+  render() {
+    return (
+      <div className="App">
+        <div>
+          <Circle number={this.state.numbers[0].number}/>
+          <Circle number={this.state.numbers[1].number}/>
+          <Circle number={this.state.numbers[2].number}/>
+          <Circle number={this.state.numbers[3].number}/>
+          <Circle number={this.state.numbers[4].number}/>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
